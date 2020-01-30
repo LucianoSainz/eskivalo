920162d5f757c624be0b80e6747b73380978704a
luciaoSainz <danielsainz28@gmail.com>
2020-01-30 09:20:37 +0100

    removing the button when it starts

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     document.getElementById("start-button").onclick = function () {
-  
+    document.querySelector(".eskivalo").style.display="none";
       game.init();
   
     };
